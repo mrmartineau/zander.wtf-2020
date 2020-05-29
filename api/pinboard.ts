56dadf536528c25bdd0f33472407581b54ca97a2
Zander Martineau <hi@zander.wtf>
2020-05-30 00:52:45 +0100

    update pinboard serverless function

--- api/pinboard.ts
+++ api/pinboard.ts
@@ -1,11 +1,8 @@
 import { NowRequest, NowResponse } from '@now/node'
-
 import axios from 'axios'
-const DOMAIN = 'https://feeds.pinboard.in'
 
 export default async (req: NowRequest, res: NowResponse) => {
-  const apiSlug = `/json/u:MrMartineau/t:${req.query.tag}/?count=10`
-  const requestPath = `${DOMAIN}${apiSlug}`
+  const requestPath = `https://feeds.pinboard.in/json/u:MrMartineau/t:${req.query.tag}/?count=10`
   const { data } = await axios(requestPath)
   res.setHeader('Content-Type', 'application/json')
   res.statusCode = 200
